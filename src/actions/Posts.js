99a0d52819f806bfb5d5e22893577cd2bd047e65
dev-ahmedali <ahmedali69325@gmail.com>
2022-04-14 01:06:41 +0600

    added proxy

--- src/actions/Posts.js
+++ src/actions/Posts.js
@@ -4,7 +4,7 @@ import * as api from "../api";
 
 export  const getPosts = () => async (dispatch) => {
     try {
-        const {data} = await api.fetchPosts()
+        const {data} = await api.fetchPost()
         dispatch({type: 'FETCH_ALL', payload: data});
     } catch (error) {
         console.log(error.message)
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Post from './Post/Post';
 import useStyles from "./styles";
+import { useSelector } from 'react-redux';
 
 const Posts = () => {
+    const post = useSelector((state) => state.posts);
+    const classes = useStyles();
+    const classes = useStyle()
     return (
         <>
             <h1>Posts</h1>
